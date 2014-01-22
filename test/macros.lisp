fd210ff48af1df987d49c0341e402bb95fd0302d
Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
2014-01-22 20:14:25 +0100

    Improved and extended tests in test/macros.lisp
    * test/macros.lisp (header): updated copyright
      (test macros.define-service.smoke): check for identical class instead
      of subclass
      (test macros.define-service.redefinition): removed; split into two
      tests
      (test macros.define-service.redefinition.documentation): new test; was
      part of `macros.define-service.redefinition'
      (test macros.define-service.redefinition.redefinition): new test; was
      part of `macros.define-service.redefinition'; check for identical
      class instead of subclass
      (specialized-class-provider): renamed specialized-provider ->
      specialized-class-provider
      (test macros.register-provider/class.smoke): adapted to renamed class;
      check for identical class instead of subclass
      (test macros.register-provider/class.conditions): new test; test
      conditions signaled by `register-provider/class'
      (mock-provider): new function; used in test for
      `register-provider/function'
      (specialized-function-provider): new class; likewise
      (test macros.register-provider/function.smoke): new test; smoke test
      for `register-provider/function'
      (test macros.register-provider/function.conditions): new test; test
      conditions signaled by `register-provider/function

--- test/macros.lisp
+++ test/macros.lisp
@@ -1,6 +1,6 @@
 ;;;; macros.lisp --- Unit tests for macros of the architecture.service-provider system.
 ;;;;
-;;;; Copyright (C) 2012, 2013 Jan Moringen
+;;;; Copyright (C) 2012, 2013, 2014 Jan Moringen
 ;;;;
 ;;;; Author: Jan Moringen <jmoringe@techfak.uni-bielefeld.de>
 
@@ -30,35 +30,48 @@
   (with-cleaned-up-service (foo)
     (define-service foo
       (:service-class specialized-service))
-    (is (typep (find-service 'foo) 'specialized-service))))
+    (is (eq (find-class 'specialized-service)
+            (class-of (find-service 'foo))))))
 
-(test macros.define-service.redefinition
-  "Test behavior of the `define-service' macro when services are
-   redefined."
+(test macros.define-service.redefinition.documentation
+  "Test behavior of the `define-service' macro when service are
+   redefined with different documentation."
 
   ;; Change documentation back and forth.
   (with-cleaned-up-service (foo)
     (define-service foo)
+
     (define-service foo
       (:documentation "foo"))
     (is (equal "foo" (documentation (find-service 'foo) t)))
+
     (define-service foo
       (:documentation nil))
-    (is (equal nil (documentation (find-service 'foo) t))))
+    (is (equal nil (documentation (find-service 'foo) t)))))
 
-  ;; Change class of service.
+(test macros.define-service.redefinition.change-class
+  "Test behavior of the `define-service' macro when services are
+   redefined with different classes."
+
+  ;; Change class of service back and forth.
   (with-cleaned-up-service (foo)
     (define-service foo)
+
     (define-service foo
       (:service-class specialized-service))
-    (is (typep (find-service 'foo) 'specialized-service))))
+    (is (eq (find-class 'specialized-service)
+            (class-of (find-service 'foo)) ))
+
+    (define-service foo)
+    (is (eq (find-class 'service-provider::standard-service)
+            (class-of (find-service 'foo))))))
 
 ;;; `register-provider/class'
 
 (defclass mock-provider ()
   ())
 
-(defclass specialized-provider (service-provider::class-provider)
+(defclass specialized-class-provider (service-provider::class-provider)
   ())
 
 (test macros.register-provider/class.smoke
@@ -72,13 +85,50 @@
   ;; Test registering and finding a provider of a specialized class.
   (with-service (foo)
     (register-provider/class 'foo :mock
-                             :provider-class 'specialized-provider
+                             :provider-class 'specialized-class-provider
                              :class          'mock-provider)
     (is (equal :mock (provider-name (find-provider 'foo :mock))))
-    (is (typep (find-provider 'foo :mock) 'specialized-provider))))
+    (is (eq (find-class 'specialized-class-provider)
+            (class-of (find-provider 'foo :mock) )))))
+
+(test macros.register-provider/class.conditions
+  "Test conditions signaled by the `register-provider/class' function."
+
+  (signals missing-service-error
+    (register-provider/class :no-such-service :does-not-matter
+      :class 'mock-provider)))
 
 ;;; `register-provider/function'
 
+(defun mock-provider ())
+
+(defclass specialized-function-provider (service-provider::function-provider)
+  ())
+
+(test macros.register-provider/function.smoke
+  "Smoke test for the `register-provider/function' function."
+
+  ;; Test registering and finding a simple provider.
+  (with-service (foo)
+    (register-provider/function 'foo :mock :function 'mock-provider)
+    (is (equal :mock (provider-name (find-provider 'foo :mock)))))
+
+  ;; Test registering and finding a provider of a specialized class.
+  (with-service (foo)
+    (register-provider/function 'foo :mock
+                                :provider-class 'specialized-function-provider
+                                :function       'mock-provider)
+    (is (equal :mock (provider-name (find-provider 'foo :mock))))
+    (is (eq (find-class 'specialized-function-provider)
+            (class-of (find-provider 'foo :mock) )))))
+
+(test macros.register-provider/function.conditions
+  "Test conditions signaled by the `register-provider/function'
+   function."
+
+  (signals missing-service-error
+    (register-provider/function :no-such-service :does-not-matter
+      :function 'mock-provider)))
 
 #+no (define-provider (foo bar)
          (class :class 'bar))
